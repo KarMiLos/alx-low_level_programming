6ef4b98be8b7bf639b033c6c61dd00d0114852de
KarMiLos <camelliusaigbogun@gmail.com>
2022-10-20 09:54:53 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,12 @@
  * _isupper - check if a character is uppercase
  * @c: Variable text
  * Return: 0 or 1
+ *
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (0);
-	else
 		return (1);
+	else
+		return (0);
 }
